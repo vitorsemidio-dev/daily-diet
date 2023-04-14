8e08b4fbe7ea030b84d6d41bd66cd0ba8180f808
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-04-14 00:02:09 -0300

    chore: setup logger

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import cookie from '@fastify/cookie'
 import fastify from 'fastify'
 
+import { envToLogger } from './configs/logger'
+import { env } from './env'
 import { authRoutes } from './routes/auth.routes'
 import { mealsRoutes } from './routes/meals.routes'
 import { usersRoutes } from './routes/users.routes'
 
-const app = fastify()
+const app = fastify({
+  logger: envToLogger[env.NODE_ENV] ?? true,
+})
 
 app.register(cookie)
 
---
+++ src/configs/logger.ts
@@ -0,0 +1,13 @@
+export const envToLogger = {
+  development: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+      },
+    },
+  },
+  production: true,
+  test: false,
+}
