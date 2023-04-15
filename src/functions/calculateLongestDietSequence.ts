ea6d28ca62838a45efe7d97be33e194bc6dd0a0a
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-04-15 12:45:23 -0300

    refactor: Simplify calculateLongestDietSequence function
    Refactor the  function to simplify its logic and improve its readability. Specifically, we removed some unnecessary code and simplified the sequence counting logic to make it easier to follow. This should make the function easier to maintain and less prone to errors in the future.

--- src/functions/calculateLongestDietSequence.ts
+++ src/functions/calculateLongestDietSequence.ts
@@ -1,26 +1,34 @@
 import { MealTable } from '../@types/knex'
 import { longestSequenceOfDays } from './longestSequenceOfDays'
 
-export function calculateLongestDietSequence(meals: MealTable[]): number {
-  const longestSequenceOfDaysWithDiet = longestSequenceOfDays(
-    meals.filter((x) => x.is_diet).map((meal) => new Date(meal.date)),
-  ).map((date) => date.toISOString().slice(0, 10))
+function filterDietMeals(meals: MealTable[]): Date[] {
+  return meals.filter((meal) => meal.is_diet).map((meal) => new Date(meal.date))
+}
 
-  const dietStatusByDay = longestSequenceOfDaysWithDiet.map((date) => {
-    const mealsOfDay = meals.filter(
-      (meal) => new Date(meal.date).toISOString().slice(0, 10) === date,
-    )
+function isSameDay(date1: Date, date2: Date): boolean {
+  return (
+    date1.getFullYear() === date2.getFullYear() &&
+    date1.getMonth() === date2.getMonth() &&
+    date1.getDate() === date2.getDate()
+  )
+}
 
-    const isDietOfDay = mealsOfDay.some((meal) => meal.is_diet)
-
-    return isDietOfDay
+function getDietStatusByDay(meals: MealTable[], dates: Date[]): boolean[] {
+  return dates.map((date) => {
+    const mealsOfDay = meals.filter((meal) => {
+      const mealDate = new Date(meal.date)
+      return isSameDay(mealDate, date)
+    })
+    return mealsOfDay.some((meal) => meal.is_diet)
   })
+}
 
+function calculateLongestSequence(statuses: boolean[]): number {
   let longestSequence = 0
   let currentSequence = 0
   let previousStatus: boolean | null = null
 
-  dietStatusByDay.forEach((status) => {
+  statuses.forEach((status) => {
     if (status && status === previousStatus) {
       currentSequence += 1
     } else {
@@ -30,7 +38,16 @@ export function calculateLongestDietSequence(meals: MealTable[]): number {
     previousStatus = status
   })
 
-  longestSequence = Math.max(longestSequence, currentSequence)
+  return Math.max(longestSequence, currentSequence)
+}
 
+export function calculateLongestDietSequence(meals: MealTable[]): number {
+  const dietMeals = filterDietMeals(meals)
+  const longestSequenceOfDaysWithDiet = longestSequenceOfDays(dietMeals)
+  const dietStatusByDay = getDietStatusByDay(
+    meals,
+    longestSequenceOfDaysWithDiet,
+  )
+  const longestSequence = calculateLongestSequence(dietStatusByDay)
   return longestSequence
 }
